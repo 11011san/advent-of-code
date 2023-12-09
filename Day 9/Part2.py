8458d985faec294d3edd84b5875ab1c7a9df1923
Patric Wedebrand <patric.wedebrand@evimeria.se>
2023-12-09 10:54:31 +0100

    day 9

---
+++ Day 9/Part2.py
@@ -0,0 +1,32 @@
+import collections
+import functools
+import math
+import re
+
+input = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
+input = input.split("\n")
+
+with open("input1.txt", "r") as file_in:
+    input = []
+    for line in file_in:
+        input.append(line)
+
+output = 0
+for row in input:
+    part = list(map(lambda x: int(x), row.split(" ")))
+    diffs = [part]
+    for level_index in range(0, len(part)):
+        level = []
+        for i in range(1, len(diffs[level_index])):
+            level.append(diffs[level_index][i] - diffs[level_index][i - 1])
+        diffs.append(level)
+        if not any(x != 0 for x in level):
+            break
+    add = 0
+    for level_index in range(len(diffs) - 2, -1, -1):
+        add = diffs[level_index][0] - add
+    output += add
+print(output)
